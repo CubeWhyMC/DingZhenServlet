05f9fed0fb24997c91e77c2244bb3bb9adaa4f3a
cubewhy <cubewhy@163.com>
2024-08-01 19:08:54 +0800

    fake token

--- src/main/java/fuck/manthe/nmsl/controller/AuthController.java
+++ src/main/java/fuck/manthe/nmsl/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.SecureRandom;
 import java.util.Objects;
 
 @RestController
@@ -19,9 +20,15 @@ public class AuthController {
     String username = System.getProperty("username");
     String password = System.getProperty("password");
 
+    private static final String ALLOWED_CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int STRING_LENGTH = 33;
+
 
     @RequestMapping(value = "/auth.php", method = {RequestMethod.GET, RequestMethod.POST})
     public String auth() throws Exception {
+        if (username == null || password == null) {
+            return generateRandomString(STRING_LENGTH);
+        }
         try (Response response = httpClient.newCall(new Request.Builder()
                 .post(RequestBody.create("email=" + username + "&password=" + password + "&hwid=FUMANTHE&v=v3&t=true", MediaType.parse("application/x-www-form-urlencoded")))
                 .url("https://www.vape.gg/auth.php")
@@ -34,9 +41,22 @@ public class AuthController {
         return "1"; // cert expired
     }
 
-    @Scheduled(cron = "0 0 0 1/1 * ? ")
-    public void updateToken() {
-        // https://www.vape.gg/auth.php
+//    @Scheduled(cron = "0 0 0 1/1 * ? ")
+//    public void updateToken() {
+//        // https://www.vape.gg/auth.php
+//
+//    }
 
+
+    public static String generateRandomString(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(ALLOWED_CHARACTERS.length());
+            sb.append(ALLOWED_CHARACTERS.charAt(index));
+        }
+
+        return sb.toString();
     }
 }
