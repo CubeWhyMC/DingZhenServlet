dafb733f78bc8090cb98c7d30528c771f6d28eac
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-08-05 21:56:22 +0800

    add entity

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,9 +27,14 @@ dependencies {
     implementation("org.projectlombok:lombok")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-//    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+//    implementation("com.mybatis-flex:mybatis-flex-spring-boot-starter:1.9.2")
+    testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
+    annotationProcessor("com.mybatis-flex:mybatis-flex-processor:1.9.2")
+    implementation("com.mybatis-flex:mybatis-flex-codegen:1.8.3")
+//    testImplementation("io.projectreactor:reactor-test")
+    runtimeOnly("com.mysql:mysql-connector-j")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 dependencyManagement {
