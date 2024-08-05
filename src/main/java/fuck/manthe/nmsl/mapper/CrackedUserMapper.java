a68cb581f2fd1db4e051abcca9dd4b697f1b2e5a
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-08-05 22:01:12 +0800

    service and mapper

---
+++ src/main/java/fuck/manthe/nmsl/mapper/CrackedUserMapper.java
@@ -0,0 +1,9 @@
+package fuck.manthe.nmsl.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import fuck.manthe.nmsl.entity.CrackedUser;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CrackedUserMapper extends BaseMapper<CrackedUser> {
+}
