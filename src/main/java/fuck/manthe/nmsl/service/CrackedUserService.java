71c7212647818ef98ad1e179104ab116087077b0
cubewhy <cubewhy@163.com>
2024-08-05 22:01:12 +0800

    service and mapper

---
+++ src/main/java/fuck/manthe/nmsl/service/CrackedUserService.java
@@ -0,0 +1,7 @@
+package fuck.manthe.nmsl.service;
+
+import com.mybatisflex.core.service.IService;
+import fuck.manthe.nmsl.entity.CrackedUser;
+
+public interface CrackedUserService extends IService<CrackedUser> {
+}
