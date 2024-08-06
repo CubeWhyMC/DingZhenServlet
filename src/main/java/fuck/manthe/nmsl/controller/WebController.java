5de14970ab7d2a70e6ca1f6be08598ec983b521c
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-08-06 19:04:34 +0800

    colddown page

--- src/main/java/fuck/manthe/nmsl/controller/WebController.java
+++ src/main/java/fuck/manthe/nmsl/controller/WebController.java
@@ -1,12 +1,25 @@
 package fuck.manthe.nmsl.controller;
 
+import fuck.manthe.nmsl.utils.Const;
+import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
+    @Resource
+    RedisTemplate<String, Long> redisTemplate;
+
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+
+    @GetMapping("colddown")
+    public String coldDown(Model model) {
+        return "colddown";
     }
 }
---
+++ src/main/java/fuck/manthe/nmsl/entity/ColdDown.java
@@ -0,0 +1,12 @@
+package fuck.manthe.nmsl.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ColdDown {
+    private Long time;
+}
