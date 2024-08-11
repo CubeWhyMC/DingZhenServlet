27b50a3dd137f8919515555d2d6a0bf68c982f84
cubewhy <cubewhy@163.com>
2024-08-11 13:00:25 +0800

    fix redeem

--- src/main/java/fuck/manthe/nmsl/service/RedeemService.java
+++ src/main/java/fuck/manthe/nmsl/service/RedeemService.java
@@ -3,7 +3,7 @@ package fuck.manthe.nmsl.service;
 import fuck.manthe.nmsl.entity.RedeemCode;
 
 public interface RedeemService {
-    RedeemCode redeem(String code);
+    RedeemCode infoOrNull(String code);
     void addCode(RedeemCode code);
 
     boolean removeCode(String code);
