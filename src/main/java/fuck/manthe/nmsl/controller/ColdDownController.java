3e8519d8b6516aec7b1e7d2d605f6bfe51c045a2
cubewhy <cubewhy@163.com>
2024-08-11 20:46:56 +0800

    fix: colddown/json

--- src/main/java/fuck/manthe/nmsl/controller/ColdDownController.java
+++ src/main/java/fuck/manthe/nmsl/controller/ColdDownController.java
@@ -5,15 +5,16 @@ import fuck.manthe.nmsl.utils.Const;
 import jakarta.annotation.Resource;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/colddown")
+@RestController("colddown")
 public class ColdDownController {
     @Resource
     RedisTemplate<String, Long> redisTemplate;
 
 
-    @GetMapping("colddown/json")
+    @GetMapping("json")
     public ColdDown coldDownJson() {
         Long next = redisTemplate.opsForValue().get(Const.COLD_DOWN);
         return new ColdDown(next);
