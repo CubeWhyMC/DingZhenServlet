3229b406033c59223fe973209b20245076ff56e1
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-08-12 14:56:40 +0800

    fix: show unavailable if global CD disabled in config

--- src/main/java/fuck/manthe/nmsl/controller/ColdDownController.java
+++ src/main/java/fuck/manthe/nmsl/controller/ColdDownController.java
@@ -3,6 +3,7 @@ package fuck.manthe.nmsl.controller;
 import fuck.manthe.nmsl.entity.ColdDown;
 import fuck.manthe.nmsl.utils.Const;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +14,12 @@ public class ColdDownController {
     @Resource
     RedisTemplate<String, Long> redisTemplate;
 
+    @Value("${share.cold-down.global.enabled}")
+    boolean globalColdDownEnabled;
 
     @GetMapping("json")
     public ColdDown coldDownJson() {
+        if (!globalColdDownEnabled) return new ColdDown(-1L);
         Long next = redisTemplate.opsForValue().get(Const.COLD_DOWN);
         return new ColdDown(next);
     }
