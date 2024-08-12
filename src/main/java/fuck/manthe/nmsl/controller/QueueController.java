8e6c1e457f463dc35db62dc72582ea37ef815ed6
cubewhy <cubewhy@163.com>
2024-08-12 09:50:19 +0800

    feat: vape account webui

--- src/main/java/fuck/manthe/nmsl/controller/QueueController.java
+++ src/main/java/fuck/manthe/nmsl/controller/QueueController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("colddown/queue")
+@RestController
+@RequestMapping("colddown/queue")
 public class QueueController {
     @Resource
     RedisTemplate<String, String> redisTemplate;
