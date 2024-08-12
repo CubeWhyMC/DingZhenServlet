1d849e73e2f43619d8cff594f88c464c70862d74
cubewhy <cubewhy@163.com>
2024-08-12 09:02:22 +0800

    feat: Analysis service

--- src/main/java/fuck/manthe/nmsl/utils/Const.java
+++ src/main/java/fuck/manthe/nmsl/utils/Const.java
@@ -4,7 +4,10 @@ public interface Const {
     String TOKEN = "vape:token:";
     String COLD_DOWN = "vape:cold-down";
     String ACCOUNT_COLD_DOWN = "vape:cold-down:account:";
+    String QUEUE = "vape:cold-down:queue";
+    // Analysis
     String TOTAL_LAUNCH = "vape:total-launch";
     String TODAY_LAUNCH = "vape:today-launch";
-    String QUEUE = "vape:cold-down:queue";
+    String TOTAL_LAUNCH_PRE_USER = "vape:today-launch-user:";
+    String TOTAL_REQUEST_AUTH_PRE_USER = "vape:today-request-auth-user:";
 }
