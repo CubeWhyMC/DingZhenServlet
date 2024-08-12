a25de009cd89166727c645619a96dd4b6c884644
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-08-12 11:13:32 +0800

    fix: get value

--- src/main/java/fuck/manthe/nmsl/service/impl/AnalysisServiceImpl.java
+++ src/main/java/fuck/manthe/nmsl/service/impl/AnalysisServiceImpl.java
@@ -2,8 +2,10 @@ package fuck.manthe.nmsl.service.impl;
 
 import fuck.manthe.nmsl.service.AnalysisService;
 import fuck.manthe.nmsl.utils.Const;
+import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -13,23 +15,28 @@ public class AnalysisServiceImpl implements AnalysisService {
     @Resource
     RedisTemplate<String, Integer> redisTemplate;
 
+    @PostConstruct
+    public void init() {
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Integer.class));
+    }
+
     @Override
     public void launchInvoked(String username) {
         // global
-        redisTemplate.opsForValue().increment(Const.TODAY_LAUNCH, 1);
-        redisTemplate.opsForValue().increment(Const.TOTAL_LAUNCH, 1);
+        redisTemplate.opsForValue().increment(Const.TODAY_LAUNCH);
+        redisTemplate.opsForValue().increment(Const.TOTAL_LAUNCH);
         // pre user
-        redisTemplate.opsForValue().increment(Const.TOTAL_LAUNCH_PRE_USER + username, 1);
+        redisTemplate.opsForValue().increment(Const.TOTAL_LAUNCH_PRE_USER + username);
     }
 
     @Override
     public void authRequested(String username) {
-        redisTemplate.opsForValue().increment(Const.TOTAL_REQUEST_AUTH_PRE_USER + username, 1);
+        redisTemplate.opsForValue().increment(Const.TOTAL_REQUEST_AUTH_PRE_USER + username);
     }
 
     @Override
     public void userRegistered() {
-        redisTemplate.opsForValue().increment(Const.TODAY_REGISTER_USER, 1);
+        redisTemplate.opsForValue().increment(Const.TODAY_REGISTER_USER);
     }
 
     @Override
