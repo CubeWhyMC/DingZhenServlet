075b3b6978057402f2b05591609816e053ef1036
cubewhy <cubewhy@163.com>
2024-08-13 09:10:01 +0800

    Remove Spring Cloud gateway

---
+++ src/main/java/fuck/manthe/nmsl/controller/OnlineConfigController.java
@@ -0,0 +1,10 @@
+package fuck.manthe.nmsl.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class OnlineConfigController {
+    // todo
+}
