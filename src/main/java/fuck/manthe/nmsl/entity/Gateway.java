84508fa3cf237f022956812db44af8c70710b977
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-08-15 08:14:36 +0800

    fix: sql

--- src/main/java/fuck/manthe/nmsl/entity/Gateway.java
+++ src/main/java/fuck/manthe/nmsl/entity/Gateway.java
@@ -1,9 +1,6 @@
 package fuck.manthe.nmsl.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,5 +18,6 @@ public class Gateway {
 
     private String name;
     private String address;
+    @Column(name = "gateway_key")
     private String key;
 }
