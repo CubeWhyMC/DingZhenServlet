6af63b65b4c8adcde90391e7c18c6914fa8e10bf
cubewhy <cubewhy@163.com>
2024-09-21 21:35:43 +0800

    fix: available is a keyword

--- src/main/java/fuck/manthe/nmsl/entity/RedeemCode.java
+++ src/main/java/fuck/manthe/nmsl/entity/RedeemCode.java
@@ -1,9 +1,6 @@
 package fuck.manthe.nmsl.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,5 +21,6 @@ public class RedeemCode implements BaseData {
 
     private String reseller;
     private String redeemer;
+    @Column(name = "is_available")
     private boolean available;
 }
