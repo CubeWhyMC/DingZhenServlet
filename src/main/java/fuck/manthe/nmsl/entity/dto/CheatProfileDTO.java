254aa04e9dd0fb751500c06c5eaa4d7f26a4b9b3
cubewhy <cubewhy@163.com>
2024-10-01 21:57:35 +0800

    feat: fake data for online.vape.gg

---
+++ src/main/java/fuck/manthe/nmsl/entity/dto/CheatProfileDTO.java
@@ -0,0 +1,10 @@
+package fuck.manthe.nmsl.entity.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CheatProfileDTO {
+    // TODO
+}
---
+++ src/main/java/fuck/manthe/nmsl/entity/dto/PrivateConfigDTO.java
@@ -0,0 +1,14 @@
+package fuck.manthe.nmsl.entity.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Builder
+public class PrivateConfigDTO {
+    List<String> friends;
+    Map<String, CheatProfileDTO> profiles;
+}
