6c6ba741086978dc26e9bce6aaf1c55f823d8d96
cubewhy <cubewhy@163.com>
2024-10-02 21:32:01 +0800

    feat: Spring Security

---
+++ src/main/java/fuck/manthe/nmsl/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package fuck.manthe.nmsl.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((conf) -> conf
+                        .anyRequest().permitAll() // TODO Replace admin password with Spring Security
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+}
