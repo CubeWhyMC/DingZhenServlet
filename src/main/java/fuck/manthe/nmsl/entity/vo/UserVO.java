4e66ef54bbe99696d7443ca2a1065fd630909b58
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-10-04 15:55:39 +0800

    feat: add admins

--- src/main/java/fuck/manthe/nmsl/entity/vo/UserVO.java
+++ src/main/java/fuck/manthe/nmsl/entity/vo/UserVO.java
@@ -1,14 +1,15 @@
-package fuck.manthe.nmsl.entity.dto;
+package fuck.manthe.nmsl.entity.vo;
 
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class UserDTO {
+public class UserVO {
     private String id;
     private String username;
     private Long expire;
+    private String role;
 
     // analysis
     private long totalLaunch;
