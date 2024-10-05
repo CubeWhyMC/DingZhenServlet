a86dd418e54ca064ac180f4df723a330f8de9aae
cubewhy <cubewhy@163.com>
2024-10-05 21:29:29 +0800

    tests

--- src/test/java/fuck/manthe/nmsl/DingZhenServletApplicationTests.java
+++ src/test/java/fuck/manthe/nmsl/DingZhenServletApplicationTests.java
@@ -1,11 +1,25 @@
 package fuck.manthe.nmsl;
 
+import fuck.manthe.nmsl.entity.User;
+import fuck.manthe.nmsl.service.OnlineConfigService;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DingZhenServletApplicationTests {
+    @Resource
+    OnlineConfigService onlineConfigService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void onlineToken() {
+        String token = "123";
+        onlineConfigService.cache(token, "admin");
+        User cachedUser = onlineConfigService.findByToken(token);
+        assert cachedUser != null && cachedUser.getUsername().equals("admin");
     }
 }
