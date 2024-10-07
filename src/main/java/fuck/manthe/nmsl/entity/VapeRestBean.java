aad2c796eb97eb72c0197c3710fc2f5703a7f432
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-10-07 13:48:50 +0800

    feat: assert token in OnlineFilter

--- src/main/java/fuck/manthe/nmsl/entity/VapeRestBean.java
+++ src/main/java/fuck/manthe/nmsl/entity/VapeRestBean.java
@@ -1,5 +1,7 @@
 package fuck.manthe.nmsl.entity;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
@@ -12,5 +14,19 @@ public record VapeRestBean<T>(T data, String error, boolean successful) {
     @Contract(" -> new")
     public static <T> @NotNull VapeRestBean<T> success() {
         return success(null);
+    }
+
+    @Contract(" -> new")
+    public static <T> @NotNull VapeRestBean<T> failedToFindAccount() {
+        return failure("Failed to find account");
+    }
+
+    @Contract
+    public static <T> @NotNull VapeRestBean<T> failure(String error) {
+        return new VapeRestBean<>(null, error, false);
+    }
+
+    public String toJson() {
+        return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);
     }
 }
