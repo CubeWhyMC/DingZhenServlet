5238f0236212068156f047f5b6d2882cca1522b8
cubewhy <cubewhy@163.com>
2024-10-13 20:49:37 +0800

    refactor: colddown API

--- src/main/java/fuck/manthe/nmsl/controller/ColdDownController.java
+++ src/main/java/fuck/manthe/nmsl/controller/ColdDownController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/colddown")
+@RequestMapping("/user/colddown")
 public class ColdDownController {
     @Resource
     VapeAccountService vapeAccountService;
