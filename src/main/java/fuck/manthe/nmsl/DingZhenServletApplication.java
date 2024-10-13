bbf62212f10c4d7051ffe57a4d66d1de26643977
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-10-13 20:51:36 +0800

    chore: reformat code

--- src/main/java/fuck/manthe/nmsl/DingZhenServletApplication.java
+++ src/main/java/fuck/manthe/nmsl/DingZhenServletApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication
 public class DingZhenServletApplication {
 
     public static void main(String[] args) {
