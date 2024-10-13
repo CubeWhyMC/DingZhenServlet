72faaa73f43c350847b50e87532ccc05ea21ecbf
cubewhy <cubewhy@163.com>
2024-10-13 20:48:15 +0800

    feat: logout

--- src/main/java/fuck/manthe/nmsl/config/SecurityConfig.java
+++ src/main/java/fuck/manthe/nmsl/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,7 +30,9 @@ public class SecurityConfig {
                 )
                 .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
                 .csrf(AbstractHttpConfigurer::disable)
-                .logout(LogoutConfigurer::permitAll);
+                .logout(conf -> conf
+                        .logoutUrl("/user/logout")
+                        .permitAll());
 
         return http.build();
     }
