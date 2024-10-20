ff3a005d69367a001ce808787a286b16f95fc3b5
cubewhy <cubewhy@163.com>
2024-10-20 11:51:55 +0800

    feat: email (unused)

--- src/main/java/fuck/manthe/nmsl/entity/vo/UserVO.java
+++ src/main/java/fuck/manthe/nmsl/entity/vo/UserVO.java
@@ -8,6 +8,7 @@ import lombok.Data;
 public class UserVO {
     private String id;
     private String username;
+    private String email;
     private String role;
     private long expire;
     private long registerTime;
